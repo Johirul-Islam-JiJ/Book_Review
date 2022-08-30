d99a13ea7032aa4524c6e1440791022346dfac25
unknown <you@example.com>
2022-08-30 17:53:44 +0600

    Update formate

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -20,7 +20,7 @@ class BookController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     *    @return \Illuminate\Http\Response
      */
     public function create()
     {
